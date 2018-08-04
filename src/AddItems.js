76368fd0d19388dfae9f3385e97a8b5ff3919c2f
Rawal Ritesh <37304640+Ritzrawal@users.noreply.github.com>
2018-08-05 01:04:17 +0530

    Update AddItems.js

--- src/AddItems.js
+++ src/AddItems.js
@@ -4,8 +4,6 @@ import DisplayList from './DisplayList';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-//import Drag from './Drag';
-
 import CssBaseline from '@material-ui/core/CssBaseline';
 var rand=require('random-key');
 
